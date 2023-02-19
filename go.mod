02a79832c4656c57ce01525aa87c90e3f588a7b6
Azizul <azizul.junaid@mceasy.co.id>
2023-02-19 19:01:23 +0700

    udpate sayhello with parametername

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/izul-mceasy/go-hello
+module github.com/izul-mceasy/go-hello/v2
 
 go 1.20
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gohello
 
-func SayHello() string {
-	return "Hello World update"
+func SayHello(name string) string {
+	return "Hello World update" + name
 }
